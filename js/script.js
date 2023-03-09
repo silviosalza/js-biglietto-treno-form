244cd6200af81d82c08a855aecdce12e7eef04db
silviosalza <silviofilipposalza@gmail.com>
2023-03-09 15:05:20 +0100

    html body

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// chiedere numero di chilometri da percorrere
+// chiedere età del passeggero
+
+// il prezzo pieno del biglietto è definito in base ai km (0.21 € al km)
+// applicare uno sconto del 20% per i minorenni
+// applicare uno sconto del 40% per gli over 65.
